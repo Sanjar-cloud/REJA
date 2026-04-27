f798e6bab6011d6fc3216f830e1dc808305969a9
Sanjarbek Karimov <sanjarbekkarimov229@gmail.com>
2026-04-27 17:05:27 +0900

    feat: mongodb ulanish

--- server.js
+++ server.js
@@ -1,55 +1,33 @@
-console.log("web serverni boshlash Bismillah");
-const express = require("express");
-const app = express();
+
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
 
-let user;
-fs.readFile("database/user.json","utf8",(err, data) =>{
-   if(err) {
-      console.log("ERROR:", err);
-   } else {
-      user = JSON.parse(data)
-   }
-});
+let db;
+const connectionStrin = "mongodb+srv://sanjarbekkarimov229_db_user:NLDfEIWgcQRL4QU5@cluster0.fg12pka.mongodb.net/Reja";        
 
+mongodb.connect(connectionStrin, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,   
+ }, (err, client ) => {
+     if(err) console.log("ERROR on connection mongodb");
+     else{
+        console.log(" mongodb connection succeed")
+        module.exports = client;
 
-//1: kirish ga bogliq codlar
-app.use(express.static("public")); // bu barcha kirib kela yotgan request larga 
-//ochiq bolishi uchun public deb set qildik
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-//2: sesion ga bogliq codlar
-
-
-//3: view ga bogliq codlar
-app.set("views", "views"); // frondet uchun kerak boladigan file larni shu view 
-// folder ichidan topsan degan buyruq
-app.set("view engine","ejs"); // 
-
-//4: routing ga bogliq codlar (frontend qismi)
-app.post("/create-item", (req, res ) => {
-   
-});
-
-app.get('/author',(req, res) => {
-    res.render("author", {user: user});
-});
-
-
-app.get("/", function(req, res) {
-   res.render("Reja");
+         const app = require("./app");
+         const server = http.createServer(app);
+         let PORT = 3000;
+         server.listen(PORT, function() {
+         console.log(`the server is running successfully pn port: ${PORT}, http://localhost:${PORT}`);
 });
 
 
 
+     }
+ });
 
 
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function() {
-    console.log(`the server is running successfully pn port: ${PORT}, http://localhost:${PORT}`);
-});
+
+
 
