1f186385954c9635a7b3863b21dc9ecc0ae6e641
sanjar <sanjarbekkarimov229@gmail.com>
2026-05-13 17:49:23 +0900

    feat: H-task

--- train.js
+++ train.js
@@ -249,20 +249,33 @@
 
 // G task 
 
-function getHighestIndex(arr) {
-    let maxValue = arr[0];
-    let maxIndex = 0;
+// function getHighestIndex(arr) {
+//     let maxValue = arr[0];
+//     let maxIndex = 0;
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] > maxValue) {
+//             maxValue = arr[i];
+//             maxIndex = i;
+//         }
+//     }
+
+//     return maxIndex;
+// }
+
+// const result = getHighestIndex([5, 21, 12, 21, 8]);
+// console.log(result);  
+
+function getPositive(arr) {
+    let result = "";
 
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > maxValue) {
-            maxValue = arr[i];
-            maxIndex = i;
+        if (arr[i] > 0) {
+            result += String(arr[i]);
         }
     }
 
-    return maxIndex;
+    return result;
 }
 
-const result = getHighestIndex([5, 21, 12, 21, 8]);
-console.log(result);  
-
+console.log(getPositive([1, -4, 2]));  // 12
